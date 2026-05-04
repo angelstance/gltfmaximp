b45e65bef3e6b2b19eb91d0d8f4679e1702ee07d
angelstance <eternityfounddead@gmail.com>
2026-05-04 05:38:27 +0400

    feat: add src

---
+++ source/veroverrides.h
@@ -0,0 +1,6 @@
+// Version resource overrides for gltfmaximp
+
+#define MAXVER_INTERNALNAME "gltfmaximp\0"
+#define MAXVER_ORIGINALFILENAME "gltfmaximp.dli\0"
+#define MAXVER_FILEDESCRIPTION "glTF/GLB Importer for 3ds Max\0"
+#define MAXVER_COMMENTS "Imports mesh geometry and node TRS animation\0"
